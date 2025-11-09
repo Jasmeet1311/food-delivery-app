c0586ccd49cd18263a7a98ccada76570234e4088
Jasmeet Kaur <82758761+Jasmeet1311@users.noreply.github.com>
2025-11-09 17:06:06 +0530

    Added Live link

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-backend-yjtb.onrender.com";
   return (
     <>
       <ToastContainer/>
